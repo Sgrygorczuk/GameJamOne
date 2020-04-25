b9151ebd56c8ddb803bff186dbd5b342e2196360
Sebastian Grygorczuk <sgrygorczuk@gmail.com>
2020-04-25 08:41:08 -0400

    Start

---
+++ desktop/src/com/packt/gamejamone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.packt.gamejamone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packt.gamejamone.GameJamOneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameJamOneGame(), config);
+	}
+}
